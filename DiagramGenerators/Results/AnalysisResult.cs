3827dd0e244b29aad76507251d7e7277afe033c9
Mat <prevelm@gmail.com>
2022-01-18 15:03:20 +0100

    analysis result

---
+++ DiagramGenerators/Results/AnalysisResult.cs
@@ -0,0 +1,33 @@
+﻿using DiagramGenerators.Nodes;
+
+using System.Collections.Generic;
+
+namespace DiagramGenerators.Results
+{
+    public interface IAnalysisResult
+    {
+        IEnumerable<IFileNode> FileNodes { get; }
+    }
+
+    public interface IEditableAnalysisResult : IAnalysisResult
+    {
+        void AddFileNode(IFileNode fileNode);
+    }
+
+    public class AnalysisResult : IEditableAnalysisResult
+    {
+        public IEnumerable<IFileNode> FileNodes => fileNodes;
+
+        private readonly List<IFileNode> fileNodes;
+
+        public AnalysisResult()
+        {
+            this.fileNodes = new List<IFileNode>();
+        }
+
+        public void AddFileNode(IFileNode fileNode)
+        {
+            this.fileNodes.Add(fileNode);
+        }
+    }
+}
---
+++ Tests/UnitTests/Suite_AnalysisResult.cs
@@ -0,0 +1,39 @@
+﻿using DiagramGenerators.Nodes;
+using DiagramGenerators.Results;
+
+using Moq;
+
+using NUnit.Framework;
+
+namespace Tests.UnitTests
+{
+    public class Suite_AnalysisResult
+    {
+        private IEditableAnalysisResult result;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.result = new AnalysisResult();
+        }
+
+        [Test]
+        public void Test001_FileNodes_IsInitialyEmpty()
+        {
+            CollectionAssert.IsEmpty(result.FileNodes);
+        }
+
+        [Test]
+        public void Test002_AddFileNode_MustAddFileNodeToFileNodes()
+        {
+            //Arrange
+            var fileNode = Mock.Of<IFileNode>();
+
+            //Act
+            result.AddFileNode(fileNode);
+
+            //Assert
+            CollectionAssert.AreEqual(new[] { fileNode }, result.FileNodes);
+        }
+    }
+}
