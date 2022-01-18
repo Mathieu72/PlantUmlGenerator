bcc4142504e3d45b04e800d9a3fc992bf5308561
Mat <prevelm@gmail.com>
2022-01-18 14:36:35 +0100

    create test data

---
+++ Tests/Data/Customer.cs
@@ -0,0 +1,23 @@
+﻿namespace Tests.Data
+{
+    public interface ICustomer
+    {
+        ICustomerDescriptor Descriptor { get; }
+        string Write();
+    }
+
+    public class Customer : ICustomer
+    {
+        public ICustomerDescriptor Descriptor { get; }
+
+        public Customer(ICustomerDescriptor descriptor)
+        {
+            Descriptor = descriptor;
+        }
+
+        public string Write()
+        {
+            return Descriptor.Write();
+        }
+    }
+}
---
+++ Tests/Data/CustomerDescriptor.cs
@@ -0,0 +1,25 @@
+﻿namespace Tests.Data
+{
+    public interface ICustomerDescriptor
+    {
+        string Name { get; }
+        bool IsVip { get; }
+        string Write();
+    }
+    public class CustomerDescriptor : ICustomerDescriptor
+    {
+        public string Name { get; }
+        public bool IsVip { get; }
+
+        public CustomerDescriptor(string name, bool isVip)
+        {
+            Name = name;
+            IsVip = isVip;
+        }
+
+        public string Write()
+        {
+            return Name;
+        }
+    }
+}
