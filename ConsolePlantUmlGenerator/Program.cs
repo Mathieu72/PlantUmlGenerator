9641491d3b66e6d81480a463cbf9665bbfde83e0
Mat <prevelm@gmail.com>
2022-01-18 11:54:35 +0100

    console project

---
+++ ConsolePlantUmlGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsolePlantUmlGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
